f2004327f3014bbe4eb6341ef59dfd91c8a24f75
Ken Taylor <taylork@alum.mit.edu>
2023-12-02 02:31:18 -0800

    Days 1 & 2

---
+++ day01/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.RegularExpressions;
+
+int sum=0;
+
+foreach (var line in File.ReadLines("input")) {
+    bool gotfirst = false;
+    int first=0, last=0;
+    foreach (var c in line) {
+        if(Char.IsDigit(c)) {
+            last = (int) Char.GetNumericValue(c);
+            if(!gotfirst) {
+                first = last;
+                gotfirst = true;
+            }
+        }
+    }
+    sum+= first*10 + last;
+    Console.WriteLine($"line: {line}, num: {first*10+last}, sum: {sum}");
+}
+
+Console.WriteLine($"Part 1 Sum: {sum}");
+
+sum=0;
+
+var nums = new Dictionary<string,int> { 
+    ["zero"] = 0,
+    ["one"] = 1,
+    ["two"] = 2,
+    ["three"] = 3,
+    ["four"] = 4,
+    ["five"] = 5,
+    ["six"] = 6,
+    ["seven"] = 7,
+    ["eight"] = 8,
+    ["nine"] = 9
+};
+
+foreach (var line in File.ReadLines("input")) {
+    bool gotfirst = false;
+    int first=0, last=0;
+    foreach (Match match in Regex.Matches(line,@"\d|zero|one|two|three|four|five|six|seven|eight|nine")) {
+        Console.WriteLine($"found: {match.Value}");
+        if(Char.IsDigit(match.Value[0])) {
+            last = (int) Char.GetNumericValue(match.Value[0]);
+        } else {
+            last = nums[match.Value];
+        }
+        if(!gotfirst) {
+            first = last;
+            gotfirst = true;
+        }
+    }
+    sum+= first*10 + last;
+    Console.WriteLine($"line: {line}, num: {first*10+last}, sum: {sum}");
+}
+
+Console.WriteLine($"Part 2 Sum: {sum}");
---
+++ day02/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+
+const int OK_RED = 12;
+const int OK_GREEN = 13;
+const int OK_BLUE = 14;
+
+int sum = 0;
+int powersum=0;
+
+foreach (var line in File.ReadLines("input")) {
+    var max_draws = new Dictionary<string,int>() {
+        ["red"] = 0,
+        ["green"] = 0,
+        ["blue"] = 0
+    };
+    Match line_parse = Regex.Match(line,@"Game (\d+): (.+)");
+
+    int game = int.Parse(line_parse.Groups[1].Value);
+    string[] turns = Regex.Split(line_parse.Groups[2].Value, ";");
+
+    foreach (var turn in turns) {
+        foreach (var draw in Regex.Split(turn, ",")) {
+            Match draw_parse = Regex.Match(draw, @"\s*(\d+)\s+(\S+)");
+            int num = int.Parse(draw_parse.Groups[1].Value);
+            string color = draw_parse.Groups[2].Value;
+
+            if (num > max_draws[color]) 
+                max_draws[color] = num;
+
+        }
+    }
+
+    Console.WriteLine($"game {game}, red {max_draws["red"]}, green {max_draws["green"]}, blue {max_draws["blue"]}");
+    if (max_draws["red"] <= OK_RED &&
+        max_draws["green"] <= OK_GREEN &&
+        max_draws["blue"] <= OK_BLUE) {
+            sum += game;
+        }
+    
+    int power = max_draws["red"]*max_draws["green"]*max_draws["blue"];
+    powersum += power;
+
+}
+
+Console.WriteLine(sum);
+Console.WriteLine(powersum);
